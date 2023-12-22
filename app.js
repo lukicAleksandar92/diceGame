71582443119ee5880cdc720e2093b23d0ad5b666
Aleksandar Lukić <116752573+lukicAleksandar92@users.noreply.github.com>
2023-12-22 18:42:24 +0100

    game rules added js

--- app.js
+++ app.js
@@ -19,6 +19,19 @@ init();
 //document.querySelector('#current-' + activePlayer) .innerHTML = '<em> ' + dice + '</em> ';
 // var x = document.querySelector('#score-0') .textContent;
 
+function showRules() {
+alert(`
+GAME RULES:
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND
+score
+- BUT, if the player rolls a 1 , all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to ' Hold ' , which means that his ROUND score gets added to his GLOBAL score. After
+that, it's the next player's turn
+-The first player to reach 20 points on GLOBAL score wins the game!
+GOOD LUCK
+`)
+}
 
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
